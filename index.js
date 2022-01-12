8901dcbb34520e999f3abb74e2f763f41e0ed65a
Francois Spies <fcspies@gmail.com>
2022-01-12 12:39:27 -0500

    commit tree 819030379085a50e980e6475cf8c959ec9a8b85a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let wins = record.find(({result}) => result === "W");
+    if(wins === undefined) {
+        return undefined;
+    }
+    return wins.year
+}
+superbowlWin(record)
